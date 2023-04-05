5eb2c8923b3acec27b09cda20212d7b8d018ae59
= <122753731+JadaWoods626@users.noreply.github.com>
2023-04-05 13:15:10 -0500

    update assignment 312

--- Python-Projects/file transfer.py
+++ Python-Projects/file transfer.py
@@ -7,29 +7,33 @@ import shutil
 class ParentWindow(Frame):
     def __init__(self, master):
         Frame.__init__(self)
-#create button to select files from source directory
-        self.sourceDir_btn = Button(text="Select Source", width=20, command=self.sourceDir)
-        self.sourceDir_btn.grid(row=0, column=0, padx=(20, 10), pady=(30, 0))
-        self.destDir_btn = Button(text="Select Destination", width=20, command=self.destDir)
-        self.destDir_btn.grid(row=1, column=0, padx=(20, 10), pady=(15, 10))
-#creates button to fransfer files
-        self.transfer_btn = Button(text="Transfer Files", width=20, command=self.transferFiles)
-        self.transfer_btn.grid(row=2, column=1, padx=(200, 0), pady=(0, 15))
-#creates entry for source directory selection
-        self.source_dir = Entry(width=75)
-        self.source_dir.grid(row=0, column=1, columnspan=2, padx=(20, 10), pady=(30, 0))
-        self.destination_dir = Entry(width=75)
-        self.destination_dir.grid(row=1, column=1, columnspan=2, padx=(20, 10), pady=(15, 10))
-#creates button to exit       
-        self.exit_btn = Button(text="Exit", width=20, command=self.exitProgram)
-        self.exit_btn.grid(row=2, column=2, padx=(10, 40), pady=(0, 15))        
+        
 #sets title of GUI
         self.master.title("File Transfer")
+#buttons and entry
+        self.sourceDir_btn = Button(text="Select Source", width=20, command=self.sourceDir)
+        self.sourceDir_btn.grid(row=0, column=0, padx=(20, 10), pady=(30, 0))
+        
+        self.source_dir = Entry(width=75)
+        self.source_dir.grid(row=0, column=1, columnspan=2, padx=(20, 10), pady=(30, 0))
+        
+        self.destDir_btn = Button(text="Select Destination", width=20, command=self.destDir)
+        self.destDir_btn.grid(row=1, column=0, padx=(20, 10), pady=(15, 10))
+        
+        self.destination_dir = Entry(width=75)
+        self.destination_dir.grid(row=1, column=1, columnspan=2, padx=(20, 10), pady=(15, 10))
+        
+        self.transfer_btn = Button(text="Transfer Files", width=20, command=self.transferFiles)
+        self.transfer_btn.grid(row=2, column=1, padx=(200, 0), pady=(0, 15))
+      
+        self.exit_btn = Button(text="Exit", width=20, command=self.exitProgram)
+        self.exit_btn.grid(row=2, column=2, padx=(10, 40), pady=(0, 15))        
+
+#functions        
     def sourceDir(self):
         selectSourceDir = tkinter.filedialog.askdirectory()
         self.source_dir.delete(0, END)
         self.source_dir.insert(0, selectSourceDir)
-
         
     def destDir(self):
         selectDestDir= tkinter.filedialog.askdirectory()
@@ -38,7 +42,7 @@ class ParentWindow(Frame):
 
     def transferFiles(self):
         source = self.source_dir.get()
-        destination = self.destination_dir.get(os.path.getmtime())
+        destination = self.destination_dir.get()
         source_files = os.listdir(source)
         for i in source_files:
             shutil.move(source + '/' + i, destination)
