7c271bc12159cc03d1e9a5e09f3112d2ad068594
= <122753731+JadaWoods626@users.noreply.github.com>
2023-04-17 19:10:29 -0500

    Assignment 415

---
+++ DjangoTwo/BlueBirdBanking/Checkbook/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from .forms import AccountForm, TransactionForm
+
+def home(request):
+    return render(request, 'checkbook/index.html')
+
+def create_account(request):
+    return render(request, 'checkbook/CreateNewAccount.html')
+
+def balance(request):
+    return render(request, 'checkbook/BalanceSheet.html')
+
+def transaction(request):
+    return render(request, 'checkbook/AddTransaction.html')
+
+# Create your views here.
+def create_account(request):
+    form = AccountForm(data=request.POST or None)
+
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    content = {'form': form}
+
+    return render(request, 'checkbook/CreateNewAccount.html', content)
+
+def transaction(request):
+    form = TransactionForm(data=request.POST or None)
+
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    content = {'form': form}
+
+    return render(request, 'checkbook/AddTransaction.html', content)
